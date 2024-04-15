8e3adb252fa4ce0214b962f528572bc9e1454e5c
DevEn <130136443+DevEn9u@users.noreply.github.com>
2024-04-15 18:00:53 +0900

    Add files via upload

---
+++ 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/05_반응형프로젝트(10일)/nexongames/js/ui-common.js
@@ -0,0 +1,21 @@
+$(function() {
+// video slider
+let mainVideo = new Swiper('.main_video .swiper', {
+  loop: true,
+  // allowTouchMove: false,
+  speed: 500,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+ });
+ 
+$('main_video', )
+})
