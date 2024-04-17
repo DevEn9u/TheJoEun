5966cbcb10f6820e98eff803ca9568eb124b05fd
DevEn <130136443+DevEn9u@users.noreply.github.com>
2024-04-17 18:12:54 +0900

    24.04.17(Tue)_class

--- 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/04_jQuery,swiper(5일)/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
+++ 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/04_jQuery,swiper(5일)/10_롯데스윗몰(슬라이더옆에텍스트있는타입,centermode,100퍼슬라이더)/js/ui-common.js
@@ -2,6 +2,10 @@ $(function() {
   let mainNew = new Swiper('.main_new .swiper', {
     // css로 크키 지정시 auto
     slidesPerView: 'auto',
+    spaceBetween: 30,
 
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
 });
