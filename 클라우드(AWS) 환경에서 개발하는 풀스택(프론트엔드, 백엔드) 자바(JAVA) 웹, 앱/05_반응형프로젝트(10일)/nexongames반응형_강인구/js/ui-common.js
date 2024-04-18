da1324348f963250402d36ed21fa14f4bf554b8a
DevEn <130136443+DevEn9u@users.noreply.github.com>
2024-04-18 18:11:24 +0900

    24.04.18_edited

--- 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/05_반응형프로젝트(10일)/nexongames반응형_강인구/js/ui-common.js
+++ 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/05_반응형프로젝트(10일)/nexongames반응형_강인구/js/ui-common.js
@@ -1,20 +1,23 @@
-$(function() {
-  $('#header .gnb').on('mouseenter', function() {
+$(function () {
+  $('#header .gnb').on('mouseenter', function () {
     $('#header').addClass('on');
   });
-  $('#header').on('mouseleave', function() {
+  $('#header').on('mouseleave', function () {
     $('#header').removeClass('on');
   });
-  
+
   // 사이드 메뉴 작동
-  $('#header .open_btn').on('click', function() {
+  $('#header .open_btn').on('click', function () {
     $('#header .m_gnb_wrap').addClass('on');
+    // 사이드 메뉴 열 때 body.on으로 만들어 body scroll 가림
+    $('body').addClass('on');
   });
-  $('#header .close_btn').on('click', function() {
+  $('#header .close_btn').on('click', function () {
     $('#header .m_gnb_wrap').removeClass('on');
+    $('body').removeClass('on');
   });
 
-  $('#header .m_gnb>li>a').on('click', function() {
+  $('#header .m_gnb>li>a').on('click', function () {
     // 사이드 메뉴 list 클릭시 depth2 토글
     $(this)
       .parent()
@@ -24,36 +27,83 @@ $(function() {
   });
 
 
-
-
-
-// video slider
-let mainVideoSlider = new Swiper('.main_video .swiper', {
-  // loop: true,
-  // speed: 500,
-  // autoplay: {
-  //   delay: 1000,
-  //   disableOnInteraction: false,
-  // },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-    formatFractionCurrent: function (number) {
-      return (number < 10 ? '0' + number : number);
+  // video slider
+  let mainVideoSlider = new Swiper('.main_video .swiper', {
+    loop: true,
+    // speed: 500,
+    watchSlidesProgress: true,
+    autoplay: {
+      delay: 10000,
+      disableOnInteraction: false,
     },
-    formatFractionTotal: function (number) {
-      return (number < 10 ? '0' + number : number);
+    // pagination: {
+    //   el: '.pagination_number',
+    //   type: 'fraction',
+    //   formatFractionCurrent: function (number) {
+    //     return (number < 10 ? '0' + number : number);
+    //   },
+    //   formatFractionTotal: function (number) {
+    //     return (number < 10 ? '0' + number : number);
+    //   },
+    //   renderFraction: function (currentClass, totalClass) {
+    //     return '<span class="' + currentClass + '"></span>' +
+    //       ' / ' +
+    //       '<span class="' + totalClass + '"></span>';
+    //   },
+    // },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
     },
-    renderFraction: function (currentClass, totalClass) {
-      return '<span class="' + currentClass + '"></span>' +
-      ' / ' +
-      '<span class="' + totalClass + '"></span>';
+    navigation: {
+      nextEl: '.swiper-button-next',
     },
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
- });
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    on: {
+      slideChange: function () {
+        let totalSlide = $('.main_video .swiper-slide').length;
+        $('.main_video .page_num .total').text(
+          totalSlide < 10 ? '0' + totalSlide : totalSlide
+        );
 
+        let idx = mainVideoSlider.realIndex + 1;
+        $('.main_video .page_num .current').text(idx < 10 ? '0' + idx : idx);
+
+        const videos = $('.main_video video');
+    
+        for (let i = 0; i < videos.length; i++) {
+          videos[i].currentTime = 0;
+          videos[i].play();
+          // console.log(videos[i], videos);
+        }
+      },
+    },
+  });
+  // slide가 바뀔 때 각 video의 재생 상태 초기화
+  // 주석 이유: 더 간단한게 작성 가능
+  // mainVideoSlider.on('slideChange', function () {
+  //   const videos = $('.main_video video');
+    
+  //   for (let i = 0; i < videos.length; i++) {
+  //     videos[i].currentTime = 0;
+  //     videos[i].play();
+  //     // videos는 배열, videos[i]는 일반 DOM return
+  //     // console.log(videos[i], videos);
+  //   };
+  // });
+  
+  // 한개를 제어할 땐 get() 메서드 사용(js 메서드로 바꿔줌)
+  // $('.main_video video').get(0).play();
+
+  // 한 개의 Swiper 안에 2개의 pagination을 만들 수 없어 새로운 Swiper 생성
+  // let paginationProgress = new Swiper('.main_video .swiper',  {
+  //   loop: true,
+  //   pagination: {
+  //     el: '.pagination_progress',
+  //     type: 'progressbar',
+  //   },
+  // });
+  // mainVideoSlider.controller.control = paginationProgress;
 });
