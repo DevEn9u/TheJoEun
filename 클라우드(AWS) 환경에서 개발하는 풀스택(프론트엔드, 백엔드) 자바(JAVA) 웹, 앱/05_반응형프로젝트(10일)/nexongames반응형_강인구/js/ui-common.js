c1824a9fb14bb897216695157c2e59dd1c3817c5
DevEn <130136443+DevEn9u@users.noreply.github.com>
2024-04-24 18:04:55 +0900

    24.04.24(Wed)_edited
    login page, corporate-governance page, tables(on corporate-governance) added

--- 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/05_반응형프로젝트(10일)/nexongames반응형_강인구/js/ui-common.js
+++ 클라우드(AWS) 환경에서 개발하는 풀스택(프론트엔드, 백엔드) 자바(JAVA) 웹, 앱/05_반응형프로젝트(10일)/nexongames반응형_강인구/js/ui-common.js
@@ -1,34 +1,30 @@
 $(function () {
-  $('#header .gnb').on('mouseenter', function () {
-    $('#header').addClass('on');
+  $("#header .gnb").on("mouseenter", function () {
+    $("#header").addClass("on");
   });
-  $('#header').on('mouseleave', function () {
-    $('#header').removeClass('on');
+  $("#header").on("mouseleave", function () {
+    $("#header").removeClass("on");
   });
 
   // 사이드 메뉴 작동
-  $('#header .open_btn').on('click', function () {
-    $('#header .m_gnb_wrap').addClass('on');
+  $("#header .open_btn").on("click", function () {
+    $("#header .m_gnb_wrap").addClass("on");
     // 사이드 메뉴 열 때 body.on으로 만들어 body scroll 가림
-    $('body').addClass('on');
+    $("body").addClass("on");
   });
-  $('#header .close_btn').on('click', function () {
-    $('#header .m_gnb_wrap').removeClass('on');
-    $('body').removeClass('on');
+  $("#header .close_btn").on("click", function () {
+    $("#header .m_gnb_wrap").removeClass("on");
+    $("body").removeClass("on");
   });
 
-  $('#header .m_gnb>li>a').on('click', function () {
+  $("#header .m_gnb>li>a").on("click", function (e) {
+    e.preventDefault();
     // 사이드 메뉴 list 클릭시 depth2 토글
-    $(this)
-      .parent()
-      .toggleClass('on')
-      .siblings()
-      .removeClass('on');
+    $(this).parent().toggleClass("on").siblings().removeClass("on");
   });
-
 
   // video slider
-  let mainVideoSlider = new Swiper('.main_video .swiper', {
+  let mainVideoSlider = new Swiper(".main_video .swiper", {
     loop: true,
     // speed: 500,
     watchSlidesProgress: true,
@@ -52,28 +48,28 @@ $(function () {
     //   },
     // },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
+      el: ".swiper-pagination",
+      type: "progressbar",
     },
     navigation: {
-      nextEl: '.swiper-button-next',
+      nextEl: ".swiper-button-next",
     },
     scrollbar: {
-      el: '.swiper-scrollbar',
+      el: ".swiper-scrollbar",
     },
     on: {
       slideChange: function () {
-        let totalSlide = $('.main_video .swiper-slide').length;
-        $('.main_video .page_num .total').text(
-          totalSlide < 10 ? '0' + totalSlide : totalSlide
+        let totalSlide = $(".main_video .swiper-slide").length;
+        $(".main_video .page_num .total").text(
+          totalSlide < 10 ? "0" + totalSlide : totalSlide
         );
 
         let idx = mainVideoSlider.realIndex + 1;
-        $('.main_video .page_num .current').text(idx < 10 ? '0' + idx : idx);
+        $(".main_video .page_num .current").text(idx < 10 ? "0" + idx : idx);
 
         // video slide 넘어갈 때 재생 초기화
-        const videos = $('.main_video video');
-    
+        const videos = $(".main_video video");
+
         for (let i = 0; i < videos.length; i++) {
           videos[i].currentTime = 0;
           videos[i].play();
@@ -86,7 +82,7 @@ $(function () {
   // 주석 이유: 더 간단한게 작성 가능
   // mainVideoSlider.on('slideChange', function () {
   //   const videos = $('.main_video video');
-    
+
   //   for (let i = 0; i < videos.length; i++) {
   //     videos[i].currentTime = 0;
   //     videos[i].play();
@@ -94,7 +90,7 @@ $(function () {
   //     // console.log(videos[i], videos);
   //   };
   // });
-  
+
   // 한개를 제어할 땐 get() 메서드 사용(js 메서드로 바꿔줌)
   // $('.main_video video').get(0).play();
 
@@ -109,11 +105,17 @@ $(function () {
   // mainVideoSlider.controller.control = paginationProgress;
 
   // main_worker 슬라이더
-  let mainWokrerSlider = new Swiper('.main_worker .swiper', {
+  let mainWokrerSlider = new Swiper(".main_worker .swiper", {
     loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
+  });
+
+  // top button
+  $("#footer .top_btn").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 });
   });
 });
